ef5a1b9ea6aca1a091f093e8b0c54c89fd778bee
Jacqueline Leykam <jleykam@gmail.com>
2014-03-16 09:50:50 +1100

    Begin refactor of script.js.

--- js/script.js
+++ js/script.js
@@ -1,189 +1,138 @@
+// Constants
+AUTHENTICATION_BEGIN_MESSAGE = "Logging in...";
+AUTHENTICATION_SUCCUESS_MESSAGE = ""; // Should never actually show.
+AUTHENTICATION_FAILURE_MESSAGE = "Incorrect password"
+PLACEHOLDER_COLOURS = [
+	'#FF6138',
+	'#FFFF9D',
+	'#BEEB9F',
+	'#79BD8F',
+	'#00A388',
+];
+
+// Globals
+authenticating = false;
+activeUser = null;
+availableUsers = [];
+
+// Gets the list of available users from lightdm and fills out the #user-list
+// element with them. Records the elements created for each user in the
+// availableUsers array.
+function generateUserList() {
+	var userList = $('#user-list');
+	for (i in lightdm.users) {
+		var user = lightdm.users[i];
+		var userListEntry = $('<div />', {class: 'user'});
+
+		userListEntry.data('user', user);
+
+		// Add an image to represent the user. If no .face image is available, use a
+		// colour from the palette.
+		var userImage = $('<div />', {class: 'bubble'});
+		if (user.image.length > 0) {
+			userImage.css({'background-image': 'url('+user.image+')'});
+		} else {
+			userImage.css({backgroundColor: PLACEHOLDER_COLOURS[i % PLACEHOLDER_COLOURS.length]});
+		}
+		userImage.appendTo(userListEntry);
+
+		// Add the user's full name if it's specified, otherwise fallback to using
+		// just their username.
+		var username = $('<div />', {class: 'user-name'});
+		if (user.display_name.length > 0) {
+			username.text(user.display_name);
+		} else {
+			username.text(user.name);
+		}
+		username.appendTo(userListEntry);
+
+		availableUsers.push(userListEntry);
+		userListEntry.appendTo(userList);
+	}
+}
+
+// Choose a user to be initially selected when the login screen loads.
+function chooseInitialSelection() {
+	if (availableUsers.length == 1) {
+		selectUser(availableUsers[0]);
+	} else {
+		// TODO: Do something sensible for multiple users. This might just be
+		// nothing.
+	}
+}
+
+// Highlights only the given user, displays the password form and begins the
+// authentication procedure.
+function selectUser(user) {
+	// Selecting a user shouldn't interrupt a login attempt.
+	if (authenticating) { return false; }
+
+	// Dim all users except for this one.
+	user.parent().children().addClass('hidden').removeClass('active');
+	user.addClass('active').removeClass('hidden');
+
+	// Show the password form, clear and focus it.
+	$('#password-container').removeClass('hidden').find('input').val('').focus();
+	$('#password').removeAttr('disabled');
+
+	activeUser = user;
+	var userData = user.data('user');
+	lightdm.start_authentication(userData.name);
+}
+
+// Attempt to authenticate the selected user with lightdm.
+function doAuthentication(password) {
+	// This shouldn't happen, but don't try to authenticate with a null user.
+	if (activeUser == null) { return false; }
+
+	authenticating = true;
+	var userData = activeUser.data('user');
+	// lightdm.start_authentication(userData.name);
+	lightdm.provide_secret(password);
+
+	// Display the message for this event and disable the form.
+	$('#authentication-message').removeClass('error').text(AUTHENTICATION_BEGIN_MESSAGE).addClass('active');
+	$('#password').attr('disabled', 'disabled');
+}
+
+function finishAuthentication() {
+	if (lightdm.is_authenticated) {
+		lightdm.login(lightdm.authentication_user, lightdm.default_session);
+	} else {
+		// Authentication failed. Reset the password form and display a message.
+		authenticating = false;
+		selectUser(activeUser);
+
+		// Fade out the current message, replace it with the new one, then eventually fade out that.
+		$('#authentication-message').removeClass('active');
+
+		setTimeout(function() {
+			$('#authentication-message').text(AUTHENTICATION_FAILURE_MESSAGE).addClass('active error');
+			setTimeout(function() {
+				$('#authentication-message').removeClass('active error');
+			}, 3000); // TODO: This might fade out the message incorrectly if it changed.
+		}, 300);
+	}
+}
+
+
 $(document).ready(function() {
-  var authenticating = false;
+	// The userlist and maybe preselect one.
+	generateUserList();
+	chooseInitialSelection();
 
-  window.authentication_complete = function () {
-    authenticating = false;
-    $('#password').removeAttr('disabled');
-
-    if (lightdm.is_authenticated) {
-      lightdm.login(lightdm.authentication_user, lightdm.default_session);
-    } else {
-      $('#authentication-message').removeClass('active');
-
-      setTimeout(function() {
-        $('#authentication-message').text('Invalid password').addClass('active error');
-
-        setTimeout(function() {
-          $('#authentication-message').removeClass('active error');
-        }, 3000);
-      }, 300);
-    }
-  }
-
-  var $user_list = $('#user-list');
-  var palette = [
-    '#FF6138',
-    '#FFFF9D',
-    '#BEEB9F',
-    '#79BD8F',
-    '#00A388',
-  ];
-
-  for (i in lightdm.users) {
-    var user = lightdm.users[i];
-    var $entry = $('<div />', {class: 'user'});
-
-    $entry.data('user', user);
-    
-    var $bubble = $('<div />', {class: 'bubble'}).css({backgroundColor: palette[i]});
-        if(user.image.length > 0) 
-            $bubble.css({'background-image': 'url('+user.image+')'});
-        $bubble.appendTo($entry);
-    var $username = $('<div />', {class: 'user-name'}).text(user.display_name).appendTo($entry);
-
-    if (user.logged_in) {
-      $('<span />', {class: 'user-logged-in'}).text('logged in').appendTo($entry);
-      $entry.addClass('logged-in');
-    }
-
-    $entry.appendTo($user_list);
-  }
-
-  $('.system-button').each(function(index) {
-    $(this).css('background-color', palette[index]);
-
-    if (!lightdm['can_' + $(this).text().toLowerCase()]) {
-      $(this).hide();
-    }
-  });
-
-  $('.system-button').on('click', function() {
-    return lightdm[$(this).text().toLowerCase()]();
-  })
-
-  $('.user').on('click', function(event, navigate) {
-    if (authenticating) return false;
-
-    var navigate = typeof navigate === 'undefined' ? 0 : navigate;
-
-    var $this = $(this);
-    var user = $this.data('user');
-
-    lightdm.cancel_timed_login();
-    lightdm.start_authentication(user.name);
-
-    if (navigate == 0 && $this.hasClass('active') && $('#password-container').hasClass('hidden')) {
-      $('#password-container').removeClass('hidden');
-
-      return false;
-    }
-
-    if (navigate == 3 && !$('#password-container').hasClass('hidden')) {
-      $('#password-container').addClass('hidden');
-
-      return false;
-    }
-
-    if (navigate != 2 && $this.hasClass('active')) {
-      if (navigate != 1 && !$('#password-container').hasClass('hidden')) {
-        $('#password-container').addClass('hidden');
-      }
-
-      $('#user-list').css({
-        left: 0,
-        right: 0
-      });
-
-      $this.parent().children().removeClass('hidden active');
-
-      return false;
-    }
-
-    var index = $this.index() + 1;
-    var total = $this.siblings().length + 1;
-    var shift = ((total + 1) / 2 - index) * $this.outerWidth(true);
-
-    $('#user-list').css({
-      left: shift,
-      right: -shift
-    });
-
-    $this.parent().children().addClass('hidden').removeClass('active');
-    $this.addClass('active').removeClass('hidden');
-
-    if (navigate != 1) {
-      $('#password-container').removeClass('hidden').find('input').val('').focus();
-    }
-  });
-
-  $('#password').on('keyup', function(e) {
-    if (e.keyCode != 13) {
-      return;
-    }
-
-    lightdm.provide_secret($(this).val());
-
-    authenticating = true;
-
-    $('#authentication-message').removeClass('error').text('Authenticating...').addClass('active');
-    $('#password').attr('disabled', 'disabled');
-  });
-
-  $(document).on('keydown mousewheel', function(e) {
-    if (authenticating) return false;
-    
-    var arrow = {left: 37, up: 38, right: 39, down: 40};
-    var users = $('#user-list .user');
-    var current = $('#user-list .active');
-    var index = Math.max(current.index(), 0);
-    
-    switch (e.originalEvent.wheelDelta || e.which) {
-      case 120:
-      case arrow.left:
-        if (current.length == 0) {
-          index = 1;
-        } else if (index == 0) {
-          break;
-        }
-
-        users.eq(index - 1).trigger('click', [1]);
-        $('#password').val('');
-        break;
-
-      case -120:
-      case arrow.right:
-        if (index == users.length - 1) break;
-
-        users.eq(index + 1).trigger('click', [1]);
-        $('#password').val('');
-        break;
-
-      case arrow.up:
-        if (current.length == 0) break;
-
-        current.trigger('click', [3]);
-        break;
-
-      case arrow.down:
-        if (current.length == 0) {
-          users.eq(index).trigger('click', [1]);
-        } else {
-          users.eq(index).trigger('click', [2]);
-        }
-        
-        break;
-    }
-  });
-
-  /*
-  var $background = $('<img />', {src: $('.user').eq(0).data('user').background, id: 'background'}).appendTo('body');
-  $background.css({
-    left: ($background.width() - $(window).width()) / 2,
-    top: ($background.height() - $(window).height()) / 2
-  });
-  */
-  
-  $('#hostname').text(lightdm.hostname);
-
-  $('body').animate({'opacity': '1'});
+	// Timed login is currently not supported, so cancel it.
+	lightdm.cancel_timed_login();
+	
+	// Register closures for events.
+	$('.user').on('click', function(event) {selectUser($(this));});
+	$('#password').on('keyup', function(e) {
+		if (e.keyCode != 13) { return; }
+		doAuthentication($(this).val());
+	})
+	window.authentication_complete = function() {finishAuthentication();};
+	
+	// Ready to go! Fade in the login screen.
+	$('body').animate({'opacity': '1'});
 });
+
